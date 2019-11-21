3c110f1aae3a1196adbece73864bf59322929889
kdjorgensen <57678631+kdjorgensen@users.noreply.github.com>
2019-11-20 17:29:10 -0700

    Add files via upload

--- Week12-Utility.py
+++ Week12-Utility.py
@@ -1,42 +1,58 @@
+#https://github.com/kdjorgensen/102-Week12.git
 #Kristian Jorgensen
 #CSCI 102A
 #Week 12 Utility
 
 # Print Output
+import string
 
 def PrintOutput(thing_to_print):
     print('OUTPUT ', thing_to_print)
 
 # LoadFile
 
-def LoadFile():
-#finsh
+def LoadFile(filename):
+    with open(filename) as f:
+        list = f.readlines()
+    return "OUTPUT ", list
 
 #UpdateString
 
-def UpdateString():
-    #finish
+def UpdateString(first_part, second_part, num):
+    print("OUTPUT ", first_part[:num] + second_part + first_part[(num + len(second_part)):])
 
 # FindWordCount
 
-def FindWordCount():
-    #finish
+def FindWordCount(list, substring):
+    return list.count(substring)
 
 # Find Score
-def ScoreFinder():
-    #finish
+def ScoreFinder(list_one, list_two, name):
+    if list_one.upper().index(name.upper()) > 0:
+        print("OUTPUT ", name, list_two[list_one.index(name)])
+    else:
+        print("OUTPUT Player Not Found")
 
 # Union
 
-def Union():
-    #finsih
+def Union(list_one, list_two):
+    return "OUTPUT ", list_one + list_two
 
 # Intersection
 
-def Intersection
-    # Finish
+def Intersection(list_one, list_two):
+    new_list = []
+    for i in range(0, len(list_one), 1):
+        if list_one in list_two:
+            new_list.append(list_one)
+    return "OUTPUT ", new_list
+
 
 # NotIn
 
-def Intersection
-    # Finish
+def NotIn(list_one, list_two):
+    new_list = []
+    for i in range(0, len(list_one), 1):
+        if list_one not in list_two:
+            new_list.append(list_one)
+    return "OUTPUT ", new_list
